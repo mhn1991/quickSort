b2989da8eedbdf3a1e9b27a56e7dc8696228a146
mhn1991 <mhn_1991@yahoo.com>
2018-07-17 15:01:07 +0100

    quick sort

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod sort;
 use sort::List;
 fn main() {
-    let mut m = List::new();
-    m.push(32);
+    let list = vec![10, 7, 8, 9, 1, 5];
+    let mut m = List::new_list(list);
+    let end = m.get_len();
+    m.sort(0,end-1);
+    m.write();
 }
--- src/sort.rs
+++ src/sort.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct List{
     list:Vec<i32>
 }
@@ -8,12 +9,51 @@ impl List{
             list:vec!()
         }
     }
-    
+
+    pub fn new_list(list:Vec<i32>)->List{
+        List{
+            list:list,
+        }
+    }
     pub fn push(&mut self,int:i32){
         self.list.push(int)
     }
 
-    pub fn sort(&self,wall:i32,current_index:i32,pivot:i32){
-        
+    pub fn sort(&mut self,low:i32,high:i32){
+        if low < high{
+            let pi = self.partition(low,high);
+            //println!("{}",pi);
+            self.sort(low,pi-1);
+            self.sort(pi+1,high);
+        }
+    }
+    
+    pub fn get_len(&self)->i32{
+        self.list.len() as i32
+    }
+    
+    fn partition(&mut self,low:i32,high:i32)->i32{
+        let pivot = self.list.get(high as usize).unwrap().clone();
+        println!("{}",pivot);
+        let mut i = (low-1);
+        for j in (low as usize)..(high-1) as usize{
+            if self.list.get(j).unwrap() <= &pivot{
+                self.list.swap(i as usize,j);
+                i+=1;
+            }
+        }
+        i+=1;
+      self.list.swap(i as usize,high as usize);
+      return i;
+    }
+    
+    pub fn clone(&self)->List{
+        List{
+            list:self.list.clone()
+        }
+    }
+
+    pub fn write(&self){
+        println!("{:?}",self.list);
     }
 }
